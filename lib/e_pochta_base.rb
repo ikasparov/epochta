abe28be0de32adb0b30cb709a6b5fa790402e01b
ikasparov <ikasparov.dev@gmail.com>
2015-12-24 11:45:25 +0300

    Fix url post request

--- lib/e_pochta_base.rb
+++ lib/e_pochta_base.rb
@@ -1,8 +1,8 @@
-module EPochtaService	
-	
+module EPochtaService
+
 	class EPochtaBase
 		attr_accessor :public_key, :private_key, :parameters
-		
+
 		# options hash: public_key, private_key
 		def initialize(options)
 			if options.is_a? Hash
@@ -15,7 +15,8 @@ module EPochtaService
 
 		def calculate_md5(params)
 			result = ''
-			params['key'] = self.public_key					
+			params['version']	= '3.0'
+			params['key'] = self.public_key
 			#stringify_keys
 			stringified_params = {}
 			params.each do |key, value|
@@ -23,7 +24,7 @@ module EPochtaService
 				# when we have array value in param, we must concatenate with 'Array' string
 				# instead of value of this array =\
 				# because of PHP origin of EPochta engine:
-				
+
 				# this is PHP example of control summ calculating, which is correct
 
 				# ksort($arrayRequest);
@@ -31,33 +32,33 @@ module EPochtaService
         #    foreach ($arrayRequest as $v)
         #       $sum .= $v; // if $v is Array then it evaluates to 'Array' string value
 
-				if value.is_a?(Array) 
+				if value.is_a?(Array)
 					stringified_params[key.to_s] = 'Array'
 					next
 				end
-				# =========				
+				# =========
 				stringified_params[key.to_s] = value.to_s
 			end
-			
-			#sort & concatenate all values    
+
+			#sort & concatenate all values
 			stringified_params.sort.each {|value|	result = result + value[1] }
 			result = result + self.private_key
 			Digest::MD5.hexdigest( result )
 		end
 
 		def form_request(params)
-			params['version']	= '3.0'
 			params['sum'] = calculate_md5 params
-			self.parameters = params.each {|k,v| v = URI.escape v.to_s }
-			
+			self.parameters = params
+			#.each {|k,v| v = URI.escape v.to_s }
+
 			url = URI("#{self.class::URL}#{params['action']}")
-			url.query = URI.encode_www_form params			
+			# url.query = URI.encode_www_form params
 			url
 		end
 
 		def exec_command(params)
-			uri = form_request(params)		
+			uri = form_request(params)
 			result = Net::HTTP.post_form(uri, self.parameters)
 		end
-	end		
+	end
 end
